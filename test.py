d9f2a3926011836233b5e056b319b0394b814af0
maksim <4403604@gmail.com>
2018-04-28 20:52:07 +0300

    corrected 24 tasks

--- test.py
+++ test.py
@@ -310,7 +310,8 @@ if __name__ == '__main__':
 
 def decorator(func):
     def wrapper():
-        return "Hi! i’am" + " " + func() + "!"
+        print('Hi! i’am {}!'.format(func()))
+        return 'Hi! i’am %s!' % (func())
     return wrapper
 
 
@@ -320,3 +321,22 @@ def a():
 
 
 print(a())
+
+# 25.Нужно функцию, которая будет имитировать звуки животных домашних - кошку, собаку, птичку.
+#  На вход подается тип животного строкой - dog, cat, parrot - на выходе издаваемые ими звуки,
+# (только для этого задания - если мы не передаем никакого животного - по умолчанию оно должно
+#  чирикать как птичка)
+
+name = str(input("Enter the name: "))
+animals = {"cat": 'may-may', "dog": 'gay-gay', "parrot": 'chirik-chirik', "pig": 'xry-xry', "cow": 'my-my'}
+
+
+def main():
+    if name in animals:
+        print(animals.get(name))
+    else:
+        print('kar-kar')
+
+
+if __name__ == '__main__':
+    main()
